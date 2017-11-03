574143599cfc536a10ff5ae6852d3dd1a9883e6f
“Kurtis-Robertsgit config --global user.email “kurtimus23@gmail.com <kurtimus23@gmail.com>
2017-11-03 15:56:45 -0600

    final push to master

---
+++ assets/javascript/app.js
@@ -0,0 +1,122 @@
+var config = {
+    apiKey: "AIzaSyA9SMpvHS8ZslCddrr9N3DCMCJjlGl8fpk",
+    authDomain: "train-assignment-51b47.firebaseapp.com",
+    databaseURL: "https://train-assignment-51b47.firebaseio.com",
+    projectId: "train-assignment-51b47",
+    storageBucket: "",
+    messagingSenderId: "520701587196"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database()
+
+var trainName = "";
+var destination = "";
+var trainTime = "";
+var frequency = "";
+
+
+
+
+$("#submit").on("click", function(event) {
+    event.preventDefault();
+
+    trainName = $("#train-name").val().trim();
+    destination = $("#destination").val().trim();
+    trainTime = $("#train-time").val().trim();
+    frequency = $("#frequency").val().trim();
+
+
+
+    database.ref().push({
+        trainName: trainName,
+        destination: destination,
+        trainTime: trainTime,
+        frequency: frequency,
+
+
+    });
+
+
+
+    var todayDate = new Date();
+
+
+
+});
+$("#remove").on("click", function() {
+    $('#train-name').val(trainName);
+    $("#destination").val(destination);
+    $("#train-time").val(trainTime);
+    $("#frequency").val(frequency);
+});
+
+
+database.ref().on("child_added", function(snapshot) {
+    var user = snapshot.val();
+    var destination = user.destination;
+
+    var row = $("<tr>")
+    var td1 = $("<td>")
+    var td2 = $("<td>")
+    var td3 = $("<td>")
+    var td4 = $("<td>")
+    var td5 = $("<td>")
+
+    td1.append(user.trainName)
+    td2.append(user.destination)
+    td3.append(user.frequency)
+    td4.append(trainTime)
+    td5.append(user.timeDiff)
+
+    row.append(td1)
+    row.append(td2)
+    row.append(td3)
+    row.append(td4)
+    row.append(td5)
+
+
+
+    // $("#tbody").append(row)
+
+
+    var dateFormat = 'HH:mm'
+    var now = moment().format(dateFormat);
+    // console.log(now)
+    var newStart = moment(user.trainTime, "hh:mm A").subtract(1, "years");
+    console.log(newStart)
+    var firstTime = newStart._i;
+
+    console.log(firstTime)
+
+    var firstTimeConverted = moment(firstTime, "hh:mm A").subtract(1, "years");
+    // console.log("First Time Converted " + firstTimeConverted);
+
+    var currentTime = moment();
+    // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    // console.log("DIFFERENCE IN TIME: " + diffTime);
+    var tRemainder = diffTime % user.frequency;
+    // console.log(tRemainder);
+    var tMinutesTillTrain = user.frequency - tRemainder;
+    // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+
+
+
+    // END TIME CONVERSTION // 
+
+    td3.text("Every " + user.frequency + " Minutes")
+    td4.text(moment(nextTrain).format("hh:mm A"))
+    td5.text(tMinutesTillTrain)
+    $("#tbody").append(row)
+
+}, function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
+});
+
+// }, function(errorObject) {
+//     // console.log("The read failed: " + errorObject.code);
+// });
